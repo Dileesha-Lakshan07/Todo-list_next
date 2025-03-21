0ad15a5a5776d5a33a868b8e74484fb836a46090
DILEESHA\dlaks <dlakshansamaraweera@gmail.com>
2025-03-21 20:51:03 +0530

    add my project and create database

---
+++ todo-next/src/app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div className="w-screen py-20 flex justify-center flex-col items-center">
+      <span>Todo App</span>
+      <h1 className="text-5xl font-extrabold uppercase mb-5 text-center">
+        <span>w/</span>Server Action
+      </h1>
+
+      <div className="flex justify-center flex-col items-center">
+        {/*input.todo*/}
+        {/*map todos*/}
+      </div>
+    </div>
+  );
+}
